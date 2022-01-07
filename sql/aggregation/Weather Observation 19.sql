20f3772ebafb7fa827537a594c711befe4993d1d
Tiffany Carruthers <tiff.l.carruthers@gmail.com>
2022-01-07 10:28:33 +0100

    Update Weather Observation 19.sql

--- sql/aggregation/Weather Observation 19.sql
+++ sql/aggregation/Weather Observation 19.sql
@@ -4,6 +4,16 @@
 Consider P1(a, c) and P2(b, d) to be two points on a 2D plane where (a, b) are the respective minimum and maximum values of Northern Latitude (LAT_N) and (c, d) are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
 Query the Euclidean Distance between points P1 and P2 and format your answer to display 4 decimal digits.
 
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+
 Euclidean Distance Formula:
 square root ((a - c)^ 2 + (b - d)^2)
 */
