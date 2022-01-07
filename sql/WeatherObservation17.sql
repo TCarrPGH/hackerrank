b1f6e6025253479e462d2e9d97466715c076411f
Tiffany Carruthers <tiff.l.carruthers@gmail.com>
2022-01-07 09:34:33 +0100

    Create WeatherObservation17.sql

---
+++ sql/WeatherObservation17.sql
@@ -0,0 +1,24 @@
+/*
+Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than . Round your answer to  decimal places.
+
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+
+SOLOUTION
+*/ 
+
+
+SELECT ROUND(LONG_W,4)
+FROM STATION 
+  WHERE LAT_N IN
+    (SELECT MIN(LAT_N)
+     FROM STATION
+        WHERE LAT_N > 38.7780
+    ) 
